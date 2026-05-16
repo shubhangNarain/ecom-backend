72030967a323152007b162f6d97adac4564c434a
shubhangNarain <snvishere@gmail.com>
2026-05-17 02:59:20 +0530

    mongo errors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  connectDb();
+app.listen(PORT, async () => {
+  await connectDb();
   console.log(`Server is running on port ${PORT}`);
 });
