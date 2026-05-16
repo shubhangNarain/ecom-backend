6fc3f0dedd4edf3334fb9b18d80541d3b5b669a1
shubhangNarain <snvishere@gmail.com>
2026-05-17 03:05:50 +0530

    added api error messages

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ message: "Error fetching products", error });
+    res.status(500).json({ message: "Error fetching products", error: error.message });
   }
 });
 
